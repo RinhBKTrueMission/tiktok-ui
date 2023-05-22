35ebc4c4ce8b506edc701962cb613e5c4c896ad2
Tran The Rinh <rinhtt@vnpt-technology.vn>
2023-05-22 22:46:51 +0700

    init tiktok

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import styles from './Header.module.scss'
+import classNames from 'classnames';
+
+const cx = classNames.bind(styles)
+function Header(){
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+            
+        </div>
+    </header>
+}
+
+export default Header;
